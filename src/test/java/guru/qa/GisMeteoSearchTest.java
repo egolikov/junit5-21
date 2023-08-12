4e8877250b3d2270b76466c0d781674ec1a16c40
egolikov <panda_93a@mail.ru>
2023-08-12 20:09:59 +0600

    upd project

--- src/test/java/guru/qa/GisMeteoSearchTest.java
+++ src/test/java/guru/qa/GisMeteoSearchTest.java
@@ -1,28 +1,18 @@
 package guru.qa;
 
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
-
-import java.util.List;
-import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.*;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SimpleWebTest {
+public class GisMeteoSearchTest {
 
     static {
         Configuration.pageLoadStrategy = "eager";
@@ -30,64 +20,20 @@ public class SimpleWebTest {
 
     @BeforeEach
     void setUp() {
-        open("https://www.google.com/");
+        open("https://www.gismeteo.ru/");
     }
 
-    @CsvSource(value = {
-            "https://selenide.org |  Selenide  | 45.34  | true",
-            "https://junit.org    |  JUnit 5   | 34.56  | false"
-    },
-    delimiter = '|')
-//    @CsvFileSource(resources = "/successfulSearchTextTest.csv")
+    @CsvFileSource(resources = "/successfulSearchGismeteoTest.csv")
     @Tags({
-            @Tag("smoke"), // BLOCKER
+            @Tag("smoke"), // HIGH
             @Tag("web")
     })
-    @DisplayName("Проверка поиска в google")
-    @ParameterizedTest(name = "В поисковой выдаче google присутствует url {0} для запроса {1}")
-    void successfulSearchTextTest(String url, String searchQuery, double i, boolean b) {
-        $("[name=q]")
-                .setValue(searchQuery)
-                .pressEnter();
+    @DisplayName("Проверка поиска в gismeteo")
+    @ParameterizedTest(name = "При вводе города {0} в поисковой выдаче на сайте gismeteo результатом поиска является {1}")
+    void successfulSearch(String nameCity, String resultCity) {
 
-
-
-        $("[id=search]").shouldHave(text(url));
+        $("input[type='search']").setValue(nameCity);
+        $(".found-section .city").shouldHave(text(nameCity)).click();
+        $(".page-title").shouldHave(text(resultCity));
     }
-
-
-
-    @ValueSource(
-            strings = {"Selenide", "JUnit 5"}
-    )
-//    @CsvFileSource(resources = "/successfulSearchTextTest.csv")
-    @Tags({
-            @Tag("smoke"), // BLOCKER
-            @Tag("web")
-    })
-    @DisplayName("Проверка поиска в google")
-    @ParameterizedTest(name = "В поисковая выдача google не пустая для запроса {0}")
-    void searchResultsShouldNotBeEmpty(String searchQuery) {
-        $("[name=q]")
-                .setValue(searchQuery)
-                .pressEnter();
-
-
-        $$("[id=search] div").shouldHave(CollectionCondition.sizeGreaterThan(0));
-    }
-
-    static Stream<Arguments> selenideSiteLocaleTest() {
-        return Stream.of(
-                Arguments.of(Locale.RU, List.of("С чего начать?", "Док", "ЧАВО", "Блог", "Javadoc", "Пользователи", "Отзывы")),
-                Arguments.of(Locale.RU, List.of("Quick start", "Docs", "FAQ", "Blog", "Javadoc", "Users", "Quotes"))
-        );
-    }
-
-    @MethodSource
-    @ParameterizedTest
-    void selenideSiteLocaleTest(Locale locale, List<String> expectedButtons) {
-        open();
-
-    }
-
 }
